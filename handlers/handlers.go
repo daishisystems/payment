4a8c7abd6ae412590c0c4d4fa5ce0f1dbf336163
Paul Mooney <paul@daishisystems.com>
2015-05-13 17:26:02 +0100

    Added Basic Couchbase Integration

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,14 +4,39 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/couchbaselabs/gocb"
 	"github.com/daishisystems/payment/todo"
 	"github.com/gorilla/mux"
 )
 
+type Card struct {
+	Number      string    `json:"number"`
+	ExpiryMonth time.Time `json:"exp_month"`
+	ExpiryYear  time.Time `json:"exp_year"`
+}
+
+type Metadata struct {
+	PaymentType string `json:"payment_type"`
+}
+
+type PaymentCard struct {
+	Id       string    `json:"id"`
+	Created  time.Time `json:"created"`
+	Card     Card      `json:"card"`
+	Metadata Metadata  `json:"metadata"`
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "Welcome!")
+	cluster, _ := gocb.Connect("couchbase://52.17.36.153")
+	bucket, _ := cluster.OpenBucket("ryanair", "")
+
+	var paymentCard PaymentCard
+	bucket.Get("01b05e21-fc3a-4049-831a-14c8a2ef667d", &paymentCard)
+
+	fmt.Printf("%s is %s\n", paymentCard.Id, paymentCard.Card.Number)
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
--- restserver/restserver.go
+++ restserver/restserver.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/couchbaselabs/gocb/gocbcore"
 	"github.com/daishisystems/payment/router"
 )
 
 func main() {
 
+	gocbcore.SetLogger(gocbcore.DefaultStdOutLogger())
 	router := router.NewRouter()
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
